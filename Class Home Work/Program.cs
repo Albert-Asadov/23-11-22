8598299a3b57f65d5f2c745f25b02ef0c10a4ae1
Albert-Asadov <alberma@code.edu.az>
2022-11-23 01:51:26 +0400

    Admin and Section

---
+++ Class Home Work/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using Class_Home_Work;
+using System.Xml;
+
+Console.WriteLine("UserName daxil edin :");
+
+string UserName = Console.ReadLine();
+
+Console.WriteLine("Password daxil edin :");
+
+string Password = Console.ReadLine();
+
+Console.WriteLine("Ugurla daxil oldunuz :");
+
+user user = new user(UserName, Password );
+
+admin admin = new admin(true, "blog section");
+
+Console.WriteLine("IsSuperAdmin:" + admin.IsSuperAdmin);
+
+Console.WriteLine("Section :" + admin.Section);
+
+Console.WriteLine("Adminin butun datalari :" + admin.Return());
---
+++ Class Home Work/admin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Home_Work
+{
+    internal class admin
+    {
+        public bool IsSuperAdmin;
+        public string Section;
+
+        public admin(bool issuperAdmin, string section)
+        {
+            IsSuperAdmin = issuperAdmin;
+            Section = section;
+
+        }
+
+        public string Return()
+        {
+            return $"{IsSuperAdmin} , {Section}";
+        }
+    }
+}
